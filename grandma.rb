2388a4886136ed06dd038922b0feb7b6e8177c75
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-03-26 03:21:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 phrase = "I LOVE YOU GRANDMA!"
 def speak_to_grandma(phrase)
-  if phrase.upcase
+  if phrase.upcase =="I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"phrase.upcase
     puts "NO, NOT SINCE 1938!"
-    elsif phrase.upcase =="I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    elsif 
   else 
     puts "HUH?! SPEAK UP, SONNY!"
   end

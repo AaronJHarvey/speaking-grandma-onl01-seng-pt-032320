48f676d8482d2b99eda73998366585324cf8c58b
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-03-26 03:27:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 
 def speak_to_grandma(phrase)
-  if phrase.upcase =="I LOVE YOU GRANDMA!"
+  if phrase.upcase =="I LOVE YOU, GRANDMA!"
     print "I LOVE YOU TOO PUMPKIN!"
     elsif phrase.upcase
     print "NO, NOT SINCE 1938!"
